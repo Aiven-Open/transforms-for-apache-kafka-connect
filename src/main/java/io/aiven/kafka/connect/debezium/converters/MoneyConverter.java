ab87e7576bdca71f395cf4e0c0c8ccdefc143055
Olena Babenko <olena@aiven.io>
2021-04-27 17:43:22 +0300

    Add Debezium Money converter (#36)
    add Debezium Money converter

---
+++ src/main/java/io/aiven/kafka/connect/debezium/converters/MoneyConverter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.connect.debezium.converters;
+
+import java.math.BigDecimal;
+import java.util.Properties;
+
+import org.apache.kafka.connect.data.SchemaBuilder;
+
+import io.debezium.spi.converter.CustomConverter;
+import io.debezium.spi.converter.RelationalColumn;
+
+public class MoneyConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
+    /**
+     * Convert money type to a correct format.
+     * Source: https://github.com/moyphilip/MoneyConverter
+     */
+    private SchemaBuilder moneySchema;
+
+    @Override
+    public void configure(final Properties props) {
+        moneySchema = SchemaBuilder.string().name(props.getProperty("schema.name"));
+    }
+
+    @Override
+    public void converterFor(final RelationalColumn column,
+                             final CustomConverter.ConverterRegistration<SchemaBuilder> registration) {
+
+        if ("money".equals(column.typeName())) {
+            registration.register(moneySchema, data -> {
+                if (data == null) {
+                    if (column.isOptional()) {
+                        return null;
+                    } else {
+                        throw new IllegalArgumentException("Money column is not optional, but data is null");
+                    }
+                }
+                if (data instanceof BigDecimal) {
+                    // Expected type
+                    return String.format("%.2f", data);
+                } else if (data instanceof Number) {
+                    return String.format("%.2f", ((Number) data).floatValue());
+                } else {
+                    throw new IllegalArgumentException("Money type should have BigDecimal type");
+                }
+
+            });
+        }
+    }
+}
---
+++ src/test/java/io/aiven/kafka/connect/debezium/converters/MoneyConverterTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2021 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.connect.debezium.converters;
+
+import java.math.BigDecimal;
+import java.util.Properties;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+
+import io.aiven.kafka.connect.debezium.converters.utils.DummyRelationalColumn;
+import io.aiven.kafka.connect.debezium.converters.utils.MoneyTestRelationalColumn;
+
+import io.debezium.spi.converter.CustomConverter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MoneyConverterTest {
+
+    private MoneyConverter transform;
+    private StubConverterRegistration registration;
+    private Properties prop;
+
+
+    @BeforeEach
+    void init() {
+        transform = new MoneyConverter();
+        registration = new StubConverterRegistration();
+        prop = new Properties();
+        prop.setProperty("schema.name", "price");
+    }
+
+    @AfterEach
+    void teardown() {
+        transform = null;
+        registration = null;
+        prop = null;
+    }
+
+    @Test
+    void shouldRegisterCorrectSchema() {
+        transform.configure(prop);
+        assertNull(registration.currFieldSchema);
+        transform.converterFor(new MoneyTestRelationalColumn(), registration);
+
+        assertEquals(registration.currFieldSchema.schema().name(), "price");
+        assertEquals(registration.currFieldSchema.schema().type(), Schema.Type.STRING);
+    }
+
+    @Test
+    void shouldDoNothingIfColumnIsNotMoney() {
+        transform.configure(prop);
+
+        transform.converterFor(new DummyRelationalColumn(), registration);
+
+        assertNull(registration.currFieldSchema);
+        assertNull(registration.currConverter);
+    }
+
+    @Test
+    void shouldFormatDataToMoneyFormat() {
+        assertNull(registration.currConverter);
+        transform.converterFor(new MoneyTestRelationalColumn(), registration);
+
+        final String result = (String) registration.currConverter.convert(BigDecimal.valueOf(103.6999));
+        assertEquals(result, "103.70");
+
+        final String result2 = (String) registration.currConverter.convert((long) 103);
+        assertEquals(result2, "103.00");
+    }
+
+    @Test
+    void shouldFailIfDataIsNotBigDecimal() {
+        assertNull(registration.currConverter);
+        transform.converterFor(new MoneyTestRelationalColumn(), registration);
+
+        final Throwable e = assertThrows(IllegalArgumentException.class,
+            () -> registration.currConverter.convert("103.6999"));
+        assertEquals(e.getMessage(), "Money type should have BigDecimal type");
+    }
+
+    @Test
+    void shouldFailIfDataIsMissing() {
+        assertNull(registration.currConverter);
+        transform.converterFor(new MoneyTestRelationalColumn(), registration);
+
+        final Throwable e = assertThrows(IllegalArgumentException.class,
+            () -> registration.currConverter.convert(null));
+        assertEquals(e.getMessage(), "Money column is not optional, but data is null");
+    }
+
+    @Test
+    void shouldDoNothingIfColumnIsOptional() {
+        transform.configure(prop);
+        final MoneyTestRelationalColumn moneyColumn = new MoneyTestRelationalColumn();
+        moneyColumn.isOptional = true;
+
+        transform.converterFor(moneyColumn, registration);
+
+        final String result = (String) registration.currConverter.convert(null);
+        assertNull(result);
+    }
+
+    class StubConverterRegistration implements CustomConverter.ConverterRegistration<SchemaBuilder> {
+        SchemaBuilder currFieldSchema;
+        CustomConverter.Converter currConverter;
+
+        @Override
+        public void register(final SchemaBuilder fieldSchema,
+                             final CustomConverter.Converter converter) {
+            currFieldSchema = fieldSchema;
+            currConverter = converter;
+        }
+    }
+}
